3d29917703d89cb2cd2c4e1ab5c950479ecb07d0
Damian Gawronski <damiangaw@gmail.com>
2023-03-28 19:50:51 +0200

    Add fromJson to model

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,9 +1,18 @@
+import 'package:simple_weather/data/remote_data_sources/weather_remote_data_source.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
+  WeatherRepository(this._weatherRemoteDataSource);
+  final WeatherRemoteDataSource _weatherRemoteDataSource;
+
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    return const WeatherModel(city: 'Warsaw', temperature: -5.5);
+    final json = await _weatherRemoteDataSource.getWeatherData(city: city);
+    if (json == null) {
+      return null;
+    }
+
+    return WeatherModel.fromJson(json);
   }
 }
